8fbd62c877e24e454d558d0f5a1e16b419c3936f
Samuel Turcotte <sturcot3@confederationcollege.ca>
2019-09-23 22:32:14 -0400

    functional to the point of calculating a single semester

--- GPA_Calculator_initial/Course.cs
+++ GPA_Calculator_initial/Course.cs
@@ -8,5 +8,78 @@ namespace GPA_Calculator_initial
 {
     class Course
     {
+        public string courseCode { get; set; }
+
+        public double creditHours { get; set; }
+
+        public double percentGrade { get; set; }
+
+        public char letterGrade { get; set; }
+
+        public double qualityPoints { get; set; }
+
+        public Course( string courseCode, double creditHours, double percentGrade)
+        {
+            this.courseCode = courseCode;
+            this.creditHours = creditHours;
+            this.percentGrade = percentGrade;
+
+            this.letterGrade = determineLetterGrade(percentGrade);
+            this.qualityPoints = determineQualityPoints(this.creditHours, this.letterGrade);
+        }
+
+        public char determineLetterGrade(double percentGrade)
+        {
+            char letterGrade;
+
+            if (percentGrade < 50.0)
+            {
+                letterGrade = 'F';
+            }
+            else if (percentGrade >= 50.0 && percentGrade < 60.0)
+            {
+                letterGrade = 'D';
+            }
+            else if (percentGrade >= 60.0 && percentGrade < 70)
+            {
+                letterGrade = 'C';
+            }
+            else if (percentGrade >= 70.0 && percentGrade < 80)
+            {
+                letterGrade = 'B';
+            }
+            else
+            {
+                letterGrade = 'A';
+            }
+
+            return letterGrade;
+        }
+
+        private double determineQualityPoints(double creditHours, char letterGrade)
+        {
+            double qualityPoints = 0;
+
+            switch (letterGrade)
+            {
+                case 'A':
+                    qualityPoints = creditHours * 4.0;
+                    break;
+                case 'B':
+                    qualityPoints = creditHours * 3.0;
+                    break;
+                case 'C':
+                    qualityPoints = creditHours * 2.0;
+                    break;
+                case 'D':
+                    qualityPoints = creditHours * 1.0;
+                    break;
+                case 'F':
+                    qualityPoints = 0.0;
+                    break;
+            }
+
+            return qualityPoints;
+        }
     }
 }
--- GPA_Calculator_initial/Semester.cs
+++ GPA_Calculator_initial/Semester.cs
@@ -8,7 +8,31 @@ namespace GPA_Calculator_initial
 {
     class Semester
     {
+        public Course[] courses { get; set; }
+        public double semesterGPA { get; set; }
 
+        public Semester(Course[] courses)
+        {
+            this.courses = courses;
+            this.semesterGPA = calculateSemesterGPA(this.courses);
+        }
+
+        public double calculateSemesterGPA(Course[] courses)
+        {
+            double GPA = 0.0;
+            double creditHouraccumulator = 0;
+            double qualityPointAccumulator = 0;
+
+            foreach (Course course in courses)
+            {
+                creditHouraccumulator += course.creditHours;
+                qualityPointAccumulator += course.qualityPoints;
+            }
+
+            GPA = qualityPointAccumulator / creditHouraccumulator;
+
+            return GPA;
+        }
 
     }
 }
