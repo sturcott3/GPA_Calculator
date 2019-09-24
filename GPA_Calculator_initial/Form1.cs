4a78349ad2023e48a2adaa69dc4ce226582dfdff
Samuel Turcotte <sturcot3@confederationcollege.ca>
2019-09-23 22:48:05 -0400

    functional to the poit of calculating cumulative gpa

--- GPA_Calculator_initial/Form1.cs
+++ GPA_Calculator_initial/Form1.cs
@@ -12,46 +12,50 @@ namespace GPA_Calculator_initial
 {
     public partial class Form1 : Form
     {
-        Course[] testCourses1;
-        Course[] testCourses2;
+        Course[] testCourses1 = new Course[]{
+                new Course("CP001" , 42.0 , 101.5),
+                new Course("CP002" , 42.0 , 100.0),
+                new Course("CP003" , 42.0 , 95.5),
+                new Course("CP004" , 42.0 , 90.0),
+                new Course("CP005" , 42.0 , 82.5),
+                new Course("CP006" , 42.0 , 94.2),
+                new Course("CP007" , 42.0 , 90.9)};
+
+        Course[] testCourses2 = new Course[]{
+                new Course("CP001" , 42.0 , 101.5),
+                new Course("CP002" , 42.0 , 100.0),
+                new Course("CP003" , 42.0 , 95.5),
+                new Course("CP004" , 42.0 , 90.0),
+                new Course("CP005" , 42.0 , 82.5),
+                new Course("CP006" , 42.0 , 94.2),
+                new Course("CP007" , 72.0 , 75.5)};
+
         Semester testSemester1;
         Semester testSemester2;
+
+        Semester[] testSemesters;
+
+        Transcript testTranscript;
 
         public Form1()
         {
             InitializeComponent();
 
-            testCourses1 = new Course[] 
-            {
-                new Course("CP001" , 42.0 , 101.5),
-                new Course("CP002" , 42.0 , 100.0),
-                new Course("CP003" , 42.0 , 95.5),
-                new Course("CP004" , 42.0 , 90.0),
-                new Course("CP005" , 42.0 , 82.5),
-                new Course("CP006" , 42.0 , 94.2),
-                new Course("CP007" , 42.0 , 90.9)
-            };
-
-            testCourses2 = new Course[]
-            {
-                new Course("CP001" , 42.0 , 101.5),
-                new Course("CP002" , 42.0 , 100.0),
-                new Course("CP003" , 42.0 , 95.5),
-                new Course("CP004" , 42.0 , 90.0),
-                new Course("CP005" , 42.0 , 82.5),
-                new Course("CP006" , 42.0 , 94.2),
-                new Course("CP007" , 72.0 , 75.5)
-          };
-
             testSemester1 = new Semester(testCourses1);
             testSemester2 = new Semester(testCourses2);
+
+            testSemesters = new Semester[] { testSemester1, testSemester2};
+
+            testTranscript = new Transcript(testSemesters);
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("Semester 1 GPA:" + testSemester1.semesterGPA);
-            Console.WriteLine("Semester 2 GPA:" + testSemester2.semesterGPA);
+            Console.WriteLine("Semester 1 GPA: " + testSemester1.semesterGPA);
+            Console.WriteLine("Semester 2 GPA: " + testSemester2.semesterGPA);
+
+            Console.WriteLine("Cumulative GPA: " + testTranscript.cumulativeGPA);
         }
     }
 }
---
+++ GPA_Calculator_initial/Transcript.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPA_Calculator_initial
+{
+    class Transcript
+    {
+        public Semester[] semesters { get; set; }
+
+        public double cumulativeGPA { get; set; }
+
+        public Transcript(Semester[] semesters)
+        {
+            this.semesters = semesters;
+
+            this.cumulativeGPA = calculateCumulativeGPA(this.semesters);
+        }
+
+        private double calculateCumulativeGPA(Semester[] semesters)
+        {
+            double cumulativeGPA = 0;
+            double creditHouraccumulator = 0;
+            double qualityPointAccumulator = 0;
+
+            foreach (Semester singleSemester in semesters)
+            {
+                foreach (Course singleCourse in singleSemester.courses)
+                {
+                    creditHouraccumulator += singleCourse.creditHours;
+                    qualityPointAccumulator += singleCourse.qualityPoints;
+                }
+            }
+
+            cumulativeGPA = qualityPointAccumulator / creditHouraccumulator;
+
+            return cumulativeGPA;
+
+        }
+
+    }
+}
