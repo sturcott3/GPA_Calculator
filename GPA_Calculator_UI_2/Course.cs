c6679c311d17a4e9ef7090a5de607e173232c158
Samuel Turcotte <sturcot3@confederationcollege.ca>
2019-10-12 18:16:39 -0400

    Removed duplicate code and unit tests from main solution. Migrated class files from original project for form-bound customizations.

---
+++ GPA_Calculator_UI_2/Course.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Major Assignment 1
+ CP-350: Critical Thinking II
+ Prof. Troy Mangatal
+ Developed by Samuel Turcotte
+ 2019-09-23*/
+namespace GPA_Calculator_2
+{
+    public class Course
+    {
+        //-_-_Properties-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+        public string Code { get; set; }
+        public double CreditHours { get; set; }
+        public bool Completed { get; set; }
+        public char LetterGrade { get; set; }
+        public double QualityPoints { get; set; }
+        public string Included { get; set; }
+
+        //Only need to expose the backing field for this property to allow added logic in the setter
+        private double percentGrade;
+        public double PercentGrade {
+            get => percentGrade;
+            set {percentGrade = value;
+                 LetterGrade = CalcLetterGrade(percentGrade);
+                 QualityPoints = CalcQualityPoints(CreditHours,LetterGrade);
+            } }
+
+        //-_-_End Properties-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+        //-_-_-_-_-_-_-_-_-__-_-_-_-__-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+        //-_-_-_-_-_-_-_-_-__-_-_-_-__-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+        //-_-_Constructors-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+        public Course(string courseCode, double creditHours, double percentGrade)
+        {
+            Code = courseCode;
+            CreditHours = creditHours;
+            PercentGrade = percentGrade;
+
+            Completed = (percentGrade == -1)? false: true;
+            
+            QualityPoints = CalcQualityPoints(creditHours, LetterGrade);
+           
+        }
+
+        //-_-_End Constructors-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+        //-_-_-_-_-_-_-_-_-__-_-_-_-__-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+        //-_-_-_-_-_-_-_-_-__-_-_-_-__-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+        //-_-_Methods-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+        public static char CalcLetterGrade(double percentGrade)
+        {//determine letter grade from percent grade
+            char letterGrade;
+            if (percentGrade == -1)
+            {
+                letterGrade = 'I';
+            }
+            else if (percentGrade > 0 && percentGrade < 50.0)
+            {//fail
+                letterGrade = 'F';
+            }
+            else if (percentGrade >= 50.0 && percentGrade < 60.0)
+            {//minimum passing
+                letterGrade = 'D';
+            }
+            else if (percentGrade >= 60.0 && percentGrade < 70)
+            {
+                letterGrade = 'C';
+            }
+            else if (percentGrade >= 70.0 && percentGrade < 80)
+            {
+                letterGrade = 'B';
+            }
+            else
+            {
+                letterGrade = 'A';
+            }
+
+            return letterGrade;
+        }
+
+        static double CalcQualityPoints(double creditHours, char letterGrade)
+        {//determine QPs from credit hours and letter grade
+            double qualityPoints = 0;
+
+            switch (letterGrade)
+            {
+                case 'A':
+                    qualityPoints = creditHours * 4.0;
+                    break;
+                case 'B':
+                    qualityPoints = creditHours * 3.0;
+                    break;
+                case 'C':
+                    qualityPoints = creditHours * 2.0;
+                    break;
+                case 'D':
+                    qualityPoints = creditHours * 1.0;
+                    break;
+                case 'F':
+                    qualityPoints = 0.0;
+                    break;
+                //default:
+                //    qualityPoints = -1;
+                //    break;
+            }
+            return qualityPoints;
+        }       
+        //-_-_End Methods-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+    }
+}
---
+++ GPA_Calculator_UI_2/Transcript.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+/*Major Assignment 1
+ CP-350: Critical Thinking II
+ Prof. Troy Mangatal
+ Developed by Samuel Turcotte
+ 2019-09-23*/
+namespace GPA_Calculator_2
+{
+    public class Transcript
+    {
+//-_-_Properties-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+        public List<Semester> Semesters { get; set; }
+        public List<Course> CourseList { get; set; }
+        public double CumulativeGPA { get; set; }
+        public List<string> Header { get; set; }
+
+//-_-_End Properties-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+//-_-_-_-_-_-_-_-_-__-_-_-_-__-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+//-_-_-_-_-_-_-_-_-__-_-_-_-__-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+//-_-_Constructors-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+        public Transcript(List<Semester> semesters, List<string> studentInfo)
+        {//to instantiate from an existing list (static data sets in TestData.cs passed in Semester Constructor)
+            Header = new List<string>(studentInfo);
+            Semesters = new List<Semester>(semesters);
+            SetupCourseList();
+            CalcCumuGPA();
+        }
+
+        public Transcript(Transcript other)
+        {//to instantiate from an existing semester (copy existing)
+            Header = new List<string>(other.Header);
+            Semesters = new List<Semester>(other.Semesters);
+            SetupCourseList();
+            CalcCumuGPA();
+        }
+   
+        public Transcript()
+        {//no-arg constructor to set default values until I add functionality to intake user data
+            Header = new List<string>() {
+            "Name: Guest Student",
+            "Class of: 2020",
+            "Campus: Thunder Bay",
+            "StudentId:999999999",
+            "email: guest@confederationcollege.ca",
+            "guest_01"};
+            Semesters = new List<Semester>();
+            CourseList = new List<Course>();
+            CumulativeGPA = 0;
+        }
+
+//-_-_End Constructors-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+//-_-_-_-_-_-_-_-_-__-_-_-_-__-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+//-_-_-_-_-_-_-_-_-__-_-_-_-__-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+//-_-_Methods-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+        public static Transcript CalcOutcome_Console(Transcript incomplete, double targetGPA)
+        {
+            //-_-Setup_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+            //create a transcript to operate on, and print the original to file
+            PrintTranscript(incomplete, "../test/" + incomplete.Header[5] + "/original.txt");
+            Transcript current  = new Transcript(incomplete);
+
+            //tracking variable (number of *different* Transcripts tested)
+            int numEx = 1;
+            double lastPrinted = 0.0;
+
+            //set each course at -1 (incomplete) to [%50 / D / recalc QPs]
+            for (int i = 0; i < current.CourseList.Count ; i++ )
+            {
+                if (current.CourseList[i].PercentGrade == -1) { current.CourseList[i].PercentGrade = 50.0; } 
+            }
+            //recalculate cumulative GPA
+            current.CalcCumuGPA();
+
+
+            //-Initial Checks-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-__-_-_-_-_-_-
+            //***If GPA meets the target, print it out and return 
+            //(means that existing grades are high enough to pass with all 50s) 
+
+            if (current.CumulativeGPA >= targetGPA)
+            {
+                Console.WriteLine("Congrats, your existing marks are high enough to reach your goal with minimums in all remaining courses");
+                PrintTranscript(current, "../test/" + current.Header[5] + "/Calculated_all50s.txt");
+                return current;
+            }
+
+            //If the transcript graduates, print it to file and set the lastPrinted tracker
+            if (current.TestGraduating())
+            {
+                PrintTranscript(current, "../test/" + current.Header[5] + "/minimum.txt");
+                lastPrinted = current.CumulativeGPA;
+            }
+
+            //Iterative Checks_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+            // if not at the target, loop until meeting requirements
+            while ((current.CumulativeGPA < targetGPA) && (!current.TestMaxChecked()))
+            {
+                //loop over all courses
+                for (int i = 0; i < current.CourseList.Count; i++)
+                {
+                    //ignore courses that are marked as completed (TODO deal with scenario where courses need to be repeated)
+                    if (!current.CourseList[i].Completed)
+                    {
+                        //increment one course by one letter grade
+                        current.CourseList[i].PercentGrade += 10.0;
+
+                        // recalculate and check the gpa. if meeting the target, print to file and return
+                        current.CalcCumuGPA();
+                        if (current.CumulativeGPA >= targetGPA)
+                        {
+                            PrintTranscript(current, "../test/"+current.Header[5]+"/final.txt");
+                            return current;
+                        }
+                        //otherwise, check if it graduates and surpasses the last printed by at least .10
+                        //if it does, print to file and continue checks
+                        else if ((current.TestGraduating()) && (current.CumulativeGPA >= (lastPrinted + 0.10)))
+                        {
+                            PrintTranscript(current, "../test/" + current.Header[5] + "/Calculated_" + numEx++ + ".txt");
+                            lastPrinted += 0.10;
+                        }
+                    }
+                }
+            }
+            PrintTranscript(current, "../test/" + current.Header[5] + "/final.txt");
+
+            //-ShutDown-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-__-_-_-_-_-_-_-_-__
+            //record number of attempts
+            Console.WriteLine(numEx + " cases tested");
+
+            //returned transcript should be the closest possibly achievable to the target without repeating courses
+            return current;
+        }
+
+
+        public void CalcCumuGPA()
+        {
+            double creditHouraccumulator = 0;
+            double qualityPointAccumulator = 0;
+            
+            foreach (Course singleCourse in CourseList)
+            {
+                if (singleCourse.PercentGrade != -1)
+                {
+                    creditHouraccumulator += singleCourse.CreditHours;
+                    qualityPointAccumulator += singleCourse.QualityPoints;
+                }
+            }
+            CumulativeGPA = qualityPointAccumulator / creditHouraccumulator;
+        }
+        
+        //test a single transcript to see whether it is eligible for graduation
+        public bool TestGraduating()
+        {
+            //start with flag set to passing
+            bool isPassing = true;
+
+            //check that each course has a passing grade
+
+                foreach (Course singleCourse in CourseList)
+                {
+                    if (singleCourse.PercentGrade < 50)
+                    {
+                        isPassing = false;
+                    }
+                }
+            
+            //check that cumulative GPA is acceptable
+            if (CumulativeGPA < 2.0)
+            {
+                isPassing = false;
+            }
+
+            //If we get past all checks without setting to false, the transcript passes
+            return isPassing;
+        }
+
+        public bool TestMaxChecked()
+        {
+            bool allCasesChecked = true;
+
+            foreach (Course course in CourseList)
+            {
+                //if the course is marked as incomplete, but does not yet have an A assigned 
+                if (!course.Completed && course.LetterGrade != 'A')
+                {//then there are still combinations to test
+                    allCasesChecked = false;
+                }
+            }
+            return allCasesChecked;
+        }
+
+        //gather all courses in all semesters in a single list 
+        //this list should only contain the highest of each course (by course code for now)
+        public void SetupCourseList()
+        {
+            CourseList = new List<Course>();
+            CourseList.Clear();
+
+            //start with every course in the Transcript
+            foreach (Semester singleSemester in Semesters)
+            {
+                foreach (Course singleCourse in singleSemester.Courses)
+                {
+                    singleCourse.Included = "IN";
+                    CourseList.Add(singleCourse);
+                }
+            }
+
+            // to get ahold of the courses one at a time, separat from the list
+            Course tester; 
+
+            for (int current = 0; current < CourseList.Count; current++)
+            {
+                tester = CourseList[current];
+
+                //look at each course, and compare it's Course Code to the tester
+                for (int idx = 0; idx < CourseList.Count; idx++)
+                {
+                    //if the course codes are equal, 
+                    if (tester.Code.Equals(CourseList[idx].Code))
+                    {
+                        //compare the marks, 
+                        if (tester.PercentGrade > CourseList[idx].PercentGrade)
+                        {
+                            //if the tester is higher, get rid of the lower grade, and mark it as EXClUDED
+                            CourseList[idx].Included = "EX";
+                            CourseList.RemoveAt(idx);
+                        }
+                    }
+                }
+            }
+        }
+        
+
+        public static void PrintTranscript(Transcript toPrint, string filename)
+        {
+            StreamWriter outputFile = new StreamWriter(filename);
+
+            int i = 1;
+
+            foreach (string line in toPrint.Header)
+            {//print student info
+                outputFile.WriteLine(line);
+            }
+
+            foreach (Semester semester in toPrint.Semesters)
+            {//print each semester one at a time
+                outputFile.WriteLine("================================================================================================");
+                outputFile.WriteLine("Semester " + i++);
+                outputFile.WriteLine("-------------------------------------------------------------------------------------");
+
+                foreach (Course course in semester.Courses)
+                {//print relevant info for each course
+                    outputFile.WriteLine("Course Code: " + course.Code);
+                    outputFile.WriteLine("Credit Hours: " + course.CreditHours);
+                    outputFile.WriteLine(course.Completed ? "Course Completed" : "Course Incomplete");
+                    outputFile.WriteLine(course.Completed ? "Percent Grade: " + course.PercentGrade : "Percent Grade: N/A");
+                    outputFile.WriteLine("Letter Grade: " + course.LetterGrade);
+                    outputFile.WriteLine(course.Completed ? "Quality Points: " + course.QualityPoints : "Quality Points: N/A");
+                    outputFile.WriteLine("-------------------------------------------------------------------------------------");
+                }
+
+                //check if the semester was completed and print its GPA if it was
+                outputFile.WriteLine(semester.Completed ? "Semester Complete" : "Semester Incomplete");
+                outputFile.WriteLine(semester.Completed ? "Semester GPA : " + semester.SemesterGPA : "Semester GPA: N/A");
+            }
+
+            //print cumulative gpa (always excludes incomplete courses)
+            outputFile.WriteLine("Current cumulative GPA: " + toPrint.CumulativeGPA);
+
+            outputFile.Close();
+        }
+
+        //-_-_End Methods-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+    }
+}
